6ac5756e2bf342af8796c4b18f9bcab43ae3a08d
Hakan Yılmaz <76787294+hakanyilmazz@users.noreply.github.com>
2023-10-20 16:56:04 +0300

    create student entity

--- src/main/java/com/example/spring/data/jpa/practice/entity/Student.java
+++ src/main/java/com/example/spring/data/jpa/practice/entity/Student.java
@@ -1,7 +1,12 @@
 package com.example.spring.data.jpa.practice.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,12 +17,23 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@Table(name = "tbl_student")
 public class Student {
 
     @Id
+    @SequenceGenerator(
+            name = "student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE
+    )
     private Long studentId;
     private String firstName;
     private String lastName;
+
+    @Column(name = "email_address")
     private String emailId;
     private String guardianName;
     private String guardianEmail;
